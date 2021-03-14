4180d72c1593cf8b32db9f2c729efe6cd29f23ca
akmarzhan1 <akmarzhan@minerva.kgi.edu>
2021-03-14 15:28:00 +0600

    Functions for max, min and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    else {
+        let min = array[0];
+        for (var i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    else {
+        let max = array[0];
+        for (var i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    else {
+        let sum = array[0];
+        for (var i = 1; i < array.length; i++) {
+            sum += array[i];
+
+        }
+        return sum / array.length;
+    }
 }
